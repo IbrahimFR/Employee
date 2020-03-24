0ab55dc3dc0cee6a93e38c1e98dbf1ea47b7d25d
Ibrahim Soliman <Ibrahim_Soliman@epam.com>
2020-03-24 11:45:11 +0100

    fixing data resource issue

--- repo/src/main/java/com/epam/employee/repo/RepoApplication.java
+++ repo/src/main/java/com/epam/employee/repo/RepoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class RepoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(RepoApplication.class, args);
     }
 }
